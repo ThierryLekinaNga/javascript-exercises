c512ff989afb1e29d40cd8b97e2f3e0716e7122a
Thierry Lekina Nga <tlekinanga@gmail.com>
2022-02-23 15:31:27 +0100

    Added javascript-exercise 1 and 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = '';
+    let i = 0;
 
+    if(num < 0)
+        return 'ERROR';
+
+    else {
+        while(i < num) {
+            str += string;
+            i++;
+        }
+    }
+    return str;
 };
+
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
