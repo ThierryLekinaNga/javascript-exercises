35a0385cd7a1f2851c27c42b05304295e1385512
Thierry Lekina Nga <tlekinanga@gmail.com>
2022-02-24 12:45:24 +0100

    Added javascript-exercises 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,43 @@
-const sumAll = function() {
+const sumAll = function(integerOne, integerTwo) {
 
+    if(integerOne < 0 || integerTwo < 0)
+        return 'ERROR';
+                
+    else if(typeof integerOne == "string" || typeof integerTwo == "string")
+        return 'ERROR';
+                
+    else if(isNaN(integerOne) || isNaN(integerTwo))
+        return 'ERROR';
+
+    else if(integerOne < integerTwo) {
+                    
+        let sum = 0;
+                
+        let i = integerOne;
+                    
+        while(i <= integerTwo) {
+                        
+            sum += i;
+                        
+            i++;
+        }
+        return sum;
+    }
+                
+    else if(integerOne > integerTwo) {
+                    
+        let sum = 0;
+                    
+        let j = integerTwo;
+                    
+        while(j <= integerOne) {
+                        
+            sum += j;
+                        
+            j++;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
