df3eb5402f8fdcc9840a14eac4caa58169a6381f
Thierry Lekina Nga <tlekinanga@gmail.com>
2022-02-24 09:43:47 +0100

    Added javascript-exercises 3 and 4.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const splitString = string.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+
+    return joinArray;
 };
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
