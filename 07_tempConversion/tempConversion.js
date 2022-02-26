30dbce45b18bb53e6fbc82d7756fa6a44d011bbe
Thierry Lekina Nga <tlekinanga@gmail.com>
2022-02-26 11:23:37 +0100

    Added javascript-exercises 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  
+  const celsius = (temp - 32) * 5/9;
+                
+    if(Number.isInteger(celsius))
+      return celsius;
+                
+    else
+      return parseFloat(celsius.toFixed(1));
+  };
 
-};
+            
+    const ctof = function(temp) {
 
-const ctof = function() {
+      const fahrenheit = temp * 9/5 + 32;
 
+      if(Number.isInteger(fahrenheit))
+        return fahrenheit;
+                
+      else
+        return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
