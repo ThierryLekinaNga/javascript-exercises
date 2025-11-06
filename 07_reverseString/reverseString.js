aababf145438321b52572e6595ce41f93241e2c4
Thierry Lekina Nga <tlekinanga@gmail.com>
2025-11-06 20:26:17 +0100

    Modified reverseString.js and reverseString.spec.js files

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str 
+        .split('')
+        .reverse()
+        .join('');
 };
-
+reverseString('hello');
+reverseString('hello');
+reverseString('123! abc! Hello, Odinite.');
+reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
