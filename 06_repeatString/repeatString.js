6b0a923d8e33cb18e635525b2e6c3bd4ebdddfb0
Thierry Lekina Nga <tlekinanga@gmail.com>
2025-11-06 19:30:19 +0100

    Modified repeatString.js and repeatString.spec.js files

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,11 +1,17 @@
 const repeatString = function(str, num) {
     let repeatedStr = "";
-    for(let i = 0; i < num; i++) {
-        repeatedStr += str;
-    }
+    if(num < 0) repeatedStr = "ERROR";
+    else { for(let i = 0; i < num; i++) repeatedStr += str; }
     return repeatedStr;
 };
+repeatString("hey", 3);
+repeatString("hello", 10);
 repeatString("hi", 1);
-
+repeatString("bye", 0);
+repeatString("goodbye", -1)
+repeatString("", 10);
+const number = Math.floor(Math.random() * 1000);
+repeatString("hey", number);
+repeatString("don't use the built-in repeat method!", 1);
 // Do not edit below this line
 module.exports = repeatString;
