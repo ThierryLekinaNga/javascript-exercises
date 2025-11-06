ee1e3c1f168a031459231693deab481627302ff3
Thierry Lekina Nga <tlekinanga@gmail.com>
2025-11-06 18:16:21 +0100

    Modified repeatString.js file

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatedStr = "";
+    for(let i = 0; i < num; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
