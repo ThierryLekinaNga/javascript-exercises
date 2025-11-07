2a8f082b0b333e0bef1021b9645a34242183e9d5
Thierry Lekina Nga <tlekinanga@gmail.com>
2025-11-07 18:57:32 +0100

    Modified removeFromArray.js and removeFromArray.spec.js files

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, val1, val2, val3, val4) {
+   return arr.filter((item) => (item !== val1 && item !== val2 && item !== val3 && item !== val4));
 };
-
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
