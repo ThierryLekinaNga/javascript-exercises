a666d798317e414e9ebde806198b49d65acbbce2
Thierry Lekina Nga <tlekinanga@gmail.com>
2025-11-09 11:53:45 +0100

    Modified sumAll.js and sumAll.spec.js files

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(a, b) {
+            let sum = 0;
+            if(a < 0 || b < 0) return "ERROR";
+            else if(Number.isInteger(a) === false || Number.isInteger(b) === false) return "ERROR";
+            else if(Number.isNaN(a) === true || Number.isNaN(b) === true) return "ERROR";
+            else {
+                if(a < b) {
+                    for(; a <= b; a++) sum += a;
+                }
+                else if(a > b) {
+                    for(; b <= a; b++) sum += b;
+                }
+            }
+            return sum;
+        }
+        sumAll(2, 4);
+        sumAll(1, 4000);
+        sumAll(123, 1);
+        sumAll(-10, 4);
+        sumAll(2.5, 4);
+        sumAll(10, "90");
+        sumAll(10, [90, 1]);
 // Do not edit below this line
 module.exports = sumAll;
