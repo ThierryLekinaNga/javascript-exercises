a0430d47de5c8861662143f4dd1fc33078423f5e
Thierry Lekina Nga <tlekinanga@gmail.com>
2025-11-10 10:00:46 +0100

    Modified leapYears.js and leapYears.spec.js files

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+            if(year % 4 !== 0) return false;
+            else if(year % 100 === 0 && year % 400 !== 0) return false;
+            else { return true; }
+        };
+        leapYears(1996);
+        leapYears(1997);
+        leapYears(34992);
+        leapYears(1900);
+        leapYears(1600);
+        leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
