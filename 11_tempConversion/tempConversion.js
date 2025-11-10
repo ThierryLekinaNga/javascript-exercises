436b5634b55ff7f0ac910a59624eb56743aab68b
Thierry Lekina Nga <tlekinanga@gmail.com>
2025-11-11 00:28:26 +0100

    Modified tempConversion.js and tempConversion.spec.js files

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  const temperatureInCelsius = (fahrenheitTemp - 32) * (5 / 9);
+  if(temperatureInCelsius !== Math.floor(temperatureInCelsius)) return +temperatureInCelsius.toFixed(1);
+  else { return temperatureInCelsius; }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const temperatureInFahrenheit = (celsiusTemp * (9 / 5) + 32);
+  if(temperatureInFahrenheit !== Math.floor(temperatureInFahrenheit)) return +temperatureInFahrenheit.toFixed(1);
+  else { return temperatureInFahrenheit; }
 };
-
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
